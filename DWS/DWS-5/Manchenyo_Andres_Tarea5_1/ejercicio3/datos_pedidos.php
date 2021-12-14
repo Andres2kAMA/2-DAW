eef5ad25536e6cb8b403861cfd9156857f46b634
Andres2kAMA <andresmancheno.alu@iespacomolla.es>
2021-12-14 20:47:55 +0100

    Ejercicio 3 acabado

---
+++ DWS/DWS-5/Manchenyo_Andres_Tarea5_1/ejercicio3/datos_pedidos.php
@@ -0,0 +1,15 @@
+<?php
+
+$bd = new PDO("mysql:dbname=pedidos;host=127.0.0.1", "root", "");
+$ins = "SELECT * FROM productos";
+$resul = $bd->query($ins);
+
+$pedidos = array();
+$cont = 0;
+
+while ($row = $resul->fetch()) {
+    $pedidos[$cont] = $row;
+    $json = json_encode($pedidos);
+    $cont++;
+}
+echo $json;
