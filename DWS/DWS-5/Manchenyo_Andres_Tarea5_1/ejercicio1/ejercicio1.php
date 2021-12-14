20e56162173607c0a75980068a880bd8bfdba563
Andres2kAMA <andresmancheno.alu@iespacomolla.es>
2021-12-14 19:53:15 +0100

    Ejercicio 1 acabado

---
+++ DWS/DWS-5/Manchenyo_Andres_Tarea5_1/ejercicio1/ejercicio1.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 1</title>
+</head>
+
+<body>
+    <script type="text/javascript">
+        var xhttp = new XMLHttpRequest();
+        setInterval(function() {
+            xhttp.open("GET", "generar_numero_aleatorio.php", false);
+            xhttp.send();
+            alert(xhttp.response);
+        }, 5000);
+    </script>
+
+</body>
+
+</html>
