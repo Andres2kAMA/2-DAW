6f2b2ad9a3f37d539c46c1e10ff97a00ea2a0c0f
Andres2kAMA <andresmancheno.alu@iespacomolla.es>
2021-12-15 17:36:27 +0100

    Funciones añadidas clase

---
+++ DWS/DWS-5/Manchenyo_Andres_Tarea5_2/php/cabecera_json.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cabecera</title>
+</head>
+
+<body>
+
+    <header>
+        <span id="cab_usuario"></span>
+        <a href="#" onclick="cargarCategorias();">Home</a>
+        <a href="#" onclick="cargarCarrito();">Carrito</a>
+        <a href="#" onclick="cerrarSesionUnaPagina();">Cerrar sesión</a>
+    </header>
+    <hr>
+</body>
+
+</html>
