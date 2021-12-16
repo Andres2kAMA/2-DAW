39e9e2b2292304f7ed0a60fe8e7a9facb4e27392
Andres2kAMA <andresmancheno.alu@iespacomolla.es>
2021-12-16 19:09:37 +0100

    Programa avanzado

--- DWC/DWC-7/DWC-7.3/Ejercicio2/js/ejercicio2.js
+++ DWC/DWC-7/DWC-7.3/Ejercicio2/js/ejercicio2.js
@@ -3,13 +3,13 @@
 /**
  * IMPORTS
  */
-import * as swApi from "./sw.js";
+import * as plantillasSW from "./plantillaSW.js";
 
 window.onload = () => {
   fetch("https://swapi.py4e.com/api/films")
     .then((Response) => Response.json())
     .then((datos) => {
-      swApi.mostrarPeliculas(datos);
+      plantillasSW.mostrarPeliculas(datos);
     })
     .catch(function (err) {
       console.log(err);
