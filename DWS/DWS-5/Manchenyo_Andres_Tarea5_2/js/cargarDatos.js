0da626785288b33f86e59a1e942a9b0d54f0329c
Andres2kAMA <andresmancheno.alu@iespacomolla.es>
2022-01-11 00:35:25 +0100

    Todos los métodos del PDF copiados

--- DWS/DWS-5/Manchenyo_Andres_Tarea5_2/js/cargarDatos.js
+++ DWS/DWS-5/Manchenyo_Andres_Tarea5_2/js/cargarDatos.js
@@ -98,4 +98,133 @@ function crearFormulario(texto, cod, funcion) {
   codigo.value = cod;
   codigo.type = "hidden";
   codigo.name = "cod";
+  var bsubmit = document.createElement("input");
+  bsubmit.type = "submit";
+  bsubmit.value = texto;
+  formu.onsubmit = function () {
+    return funcion(this);
+  };
+  formu.appendChild(unidades);
+  formu.appendChild(codigo);
+  formu.appendChild(bsubmit);
+  return formu;
+}
+
+function cargarCarrito() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      var contenido = document.getElementById("contenido");
+      contenido.innerHTML = "";
+      var titulo = document.getElementById("titulo");
+      titulo.innerHTML = "Carrito de la compra";
+      try {
+        var filas = JSON.parse(this.responseText);
+        tabla = crearTablaCarrito(filas);
+        contenido.appendChild(tabla);
+        var procesar = document.createElement("a");
+        procesar.href = "#";
+        procesar.innerHTML = "Realizar pedido";
+        procesar.onclick = function () {
+          return procesarPedido();
+        };
+        contenido.appendChild(procesar);
+      } catch (e) {
+        var mensaje = document.createElement("p");
+        mensaje.innerHTML = "Todavía no tiene productos";
+        contenido.appendChild(mensaje);
+      }
+    }
+  };
+  xhttp.open("GET", "carrito_json.php", true);
+  xhttp.send();
+  return false;
+}
+
+function crearTablaCarrito(productos) {
+  var tabla = document.createElement("table");
+  var cabecera = crear_fila(
+    ["Código", "Nombre", "Descripción", "Unidades", "Eliminar"],
+    "th"
+  );
+  tabla.appendChild(cabecera);
+  for (let i = 0; i < productos.length; i++) {
+    formu = crearFormulario(
+      "Eliminar",
+      productos[i].CodProd,
+      eliminarProductos
+    );
+    fila = crear_fila(
+      [
+        productos[i].CodProd,
+        productos[i].Nombre,
+        productos[i].Descripcion,
+        productos[i].unidades,
+      ],
+      "td"
+    );
+    celda_form = document.createElement("td");
+    celda_form.appendChild(formu);
+    fila.appendChild(celda_form);
+    tabla.appendChild(fila);
+  }
+  return tabla;
+}
+
+function anadirProductos(formulario) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      alert("Producto añadido con éxito");
+      cargarCarrito();
+    }
+  };
+  var params =
+    "cod=" +
+    formulario.elements["cod"].value +
+    "&unidades=" +
+    formulario.elements["unidades"].value;
+  xhttp.open("POST", "anadir_json.php", true);
+  xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhttp.send(params);
+  return false;
+}
+
+function eliminarProductos(formulario) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      cargarCarrito();
+      alert("Producto eliminado con éxito");
+    }
+  };
+  var params =
+    "cod=" +
+    formulario.elements["cod"].value +
+    "&unidades=" +
+    formulario.elements["unidades"].value;
+  xhttp.open("POST", "eliminar_json.php", true);
+  xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhttp.send(params);
+  return false;
+}
+
+function procesarPedido() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      var contenido = document.getElementById("contenido");
+      contenido.innerHTML = "";
+      var titulo = document.getElementById("titulo");
+      titulo.innerHTML = "TRUE";
+      if ((this.responseText = "TRUE")) {
+        contenido.innerHTML = "Pedido realizado";
+      } else {
+        contenido.innerHTML = "Error al procesar el pedido";
+      }
+    }
+  };
+  xhttp.open("GET", "procesar_pedido_json.php", true);
+  xhttp.send();
+  return false;
 }
