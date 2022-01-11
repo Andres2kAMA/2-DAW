f991e3b80da02a481f91235a292047b28ddcdcaa
Andres2kAMA <andresmancheno.alu@iespacomolla.es>
2022-01-11 20:41:55 +0100

    archivo creado

--- DWC/DWC-8/DWC-8.2/js/funciones_Firebase.js
+++ DWC/DWC-8/DWC-8.2/js/funciones_Firebase.js
@@ -1,1 +1,8 @@
 "use strict";
+
+async function obtenerProductos() {
+  const productos = await getDocs(productosCollection);
+  productos.docs.map((producto) => {
+    console.log(producto.data());
+  });
+}
