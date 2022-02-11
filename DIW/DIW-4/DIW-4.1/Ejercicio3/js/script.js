4d8e54fee4f36fefab83c974aae5641c8f288764
Andres2kAMA <andresmancheno.alu@iespacomolla.es>
2022-02-11 16:00:20 +0100

    DIW4-4.3

--- DIW/DIW-4/DIW-4.1/Ejercicio3/js/script.js
+++ DIW/DIW-4/DIW-4.1/Ejercicio3/js/script.js
@@ -1,3 +1,31 @@
 "use strict";
 
-window.onload = () => {};
+window.onload = () => {
+  document.getElementById("justify").addEventListener(
+    "input",
+    function () {
+      let valor = document.getElementById("justify").value;
+      modificarJustifyCuadricula(valor);
+      modificarTamanyoElemento(valor);
+    },
+    false
+  );
+
+  document.getElementById("align").addEventListener(
+    "input",
+    function () {
+      let valor = document.getElementById("align").value;
+      modificarAlignCuadricula(valor);
+      modificarTamanyoElemento(valor);
+    },
+    false
+  );
+
+  function modificarJustifyCuadricula(valor) {
+    document.getElementById("cuadricula").style.justifyItems = `${valor}`;
+  }
+
+  function modificarAlignCuadricula(valor) {
+    document.getElementById("cuadricula").style.alignItems = `${valor}`;
+  }
+};
